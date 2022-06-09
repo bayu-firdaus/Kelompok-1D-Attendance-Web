883a364a31957a98b76521968dd1056b4e3c9ee5
bayu-firdaus <firdausbay28@gmail.com>
2022-06-09 19:47:49 +0700

    Kelompok 1

---
+++ resources/lang/vendor/adminlte/en/adminlte.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Nama Lengkap',
+    'email'                       => 'Email',
+    'password'                    => 'Kata Sandi',
+    'retype_password'             => 'Masukkan Ulang Kata Sandi',
+    'remember_me'                 => 'Ingatkan Saya',
+    'register'                    => 'Daftar',
+    'register_a_new_membership'   => 'Daftar Pengguna Baru',
+    'i_forgot_my_password'        => 'Lupa Kata Sandi',
+    'i_already_have_a_membership' => 'Saya Sudah Punya Akun',
+    'sign_in'                     => 'Masuk',
+    'log_out'                     => 'Keluar',
+    'toggle_navigation'           => 'Toggle navigation',
+    'login_message'               => 'Silahkan Masuk Terlebih Dahulu',
+    'register_message'            => 'Daftar Pengguna Baru',
+    'password_reset_message'      => 'Atur Ulang Password',
+    'reset_password'              => 'Atur Ulang Password',
+    'send_password_reset_link'    => 'Kirim Tautan Atur Ulang Kata Sandi',
+    'verify_message'              => 'Akun Anda Memerlukan Verifikasi',
+    'verify_email_sent'           => 'Tautan Verifikasi Baru Telah Dikirim Ke Alamat Email Anda.',
+    'verify_check_your_email'     => 'Sebelum Melanjutkan, Harap Periksa Email Anda Untuk Tautan Verifikasi.',
+    'verify_if_not_recieved'      => 'Jika Anda Tidak Menerima Email',
+    'verify_request_another'      => 'Klik Di Sini Untuk Meminta Yang Lain',
+];
